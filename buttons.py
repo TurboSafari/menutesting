8d8a439fe44823a4d297dad2497ef254c718daac
TurboSafari <stickum13@gmail.com>
2016-06-28 21:55:46 -0500

    Add files via upload

--- buttons.py
+++ buttons.py
@@ -4,7 +4,10 @@ class button(object):
     def __init__(self,text,x,y,width,height,alpha=255):
         self._text = text
         self._bcolor = (0,0,0)
+        self._acolor = (150,150,150)
+        self._icolor = (0,0,0)
         self._tcolor = (255,255,255)
+        self._hover = False
         self._x = x
         self._y = y
         self._width = width
@@ -22,16 +25,31 @@ class button(object):
         self.text_rect.center = ((self._x+(self._width/2)),(self._y+(self._height/2)))
         display.blit(self.text_surf,self.text_rect)
 
-    def checkClicked(self, cur):
+    def checkClicked(self):
+        cur = pygame.mouse.get_pos()
         if self._x + self._width > cur[0] > self._x and self._y + self._height > cur[1] > self._y:
             return(True)
         else:
             return(False)
+    def setHover(self, color, active = False):
+        self._hover = active
+        self._hColor = color
+        cur = pygame.mouse.get_pos()
+        if self._x + self._width > cur[0] > self._x and self._y + self._height > cur[1] > self._y:
+            self._bcolor = self._acolor
+        else:
+            self._bcolor = self._icolor
+        
     def setTextColor(self, color):
         self._tcolor = color
     def setBackColor(self, color):
         self._bcolor = color
+        self._icolor = color
     def setFont(self, font):
         self._font = font
     def setAlpha(self, alpha):
         self._alpha = alpha
+    def setX(self, x):
+        self._x = x
+    def setY(self, y):
+        self._y = y
--- menutesting.py
+++ menutesting.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-import pygame,sys,time,random
-from buttons import button
+import pygame,sys,time,random, buttons
 
 # menu testing
 
@@ -27,32 +26,6 @@ large_font = pygame.font.SysFont('comicsansms', 80)
 fps = 30
 clock = pygame.time.Clock()
 
-def drawScrollWindow(slide_pos):
-    width = 165
-    height = 145
-    x = (display_width-width-50)
-    y = (display_height-height-50)
-    s = pygame.Surface((width,height))
-    bag_slot = []
-    for i in range(60):
-        bag_slot.append(str(i))
-    bag_length = len(bag_slot)
-    bar_size = ((height-40)/((bag_length/4)-3))+1
-    s.set_alpha(200)
-    s.fill(gray)
-    game_display.blit(s,(x,y))
-
-    drawButton('',x+width-20,y+20+(slide_pos*bar_size),15,bar_size,black,black) #draw slide bar
-
-    slide_pos += drawSlidBar('/\\',x+width-20,y+5,15,15,black,'up',slide_pos,bag_length) #draw scroll arrow
-    slide_pos += drawSlidBar('\\/',x+width-20,y+height-20,15,15,black,'down',slide_pos,bag_length)
-    
-    for iy in range(4):
-        for ix in range(4):
-            #draw buttons in window aka back pack slots special event handling to come
-            drawButton(bag_slot[(iy*4)+(slide_pos*4)+ix],(x+5+(ix*35)),(y+5+(iy*35)),30,30,black,lblack)
-    return(slide_pos)
-    
 def drawMenu():
     # draw main menu
     # possibly refined to get rid of hard coding
@@ -62,75 +35,61 @@ def drawMenu():
     s.set_alpha(200)                # alpha level
     s.fill(gray)                    # this fills the entire surface
     game_display.blit(s, (x,y))     # adds to screen
-
-    butt = button('Continue',x+5,y+5,140,30)
-    butt.setFont(small_font)
-    butt.setTextColor((0,255,0))
-    butt.setBackColor((0,0,255))
-    butt.setAlpha(50)
-    butt.drawButton(game_display)
-    cur = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if click[0] == 1:
-        if butt.checkClicked(cur):
-            print 'clicked'
-
-
+    butt = []
+    for i in range(5):
+        butt.append(buttons.button('Continue',x+5,y+5+(i*35),140,30))
+        butt[i].setFont(small_font)
+        butt[i].setTextColor((0,255,0))
+        butt[i].setBackColor((0,0,255))
+        butt[i].setHover(gray,True)
+        butt[i].setAlpha(50)
+        butt[i].drawButton(game_display)
+        cur = pygame.mouse.get_pos()            
+        click = pygame.mouse.get_pressed()
+        if pygame.mouse.get_pressed() == (1,0,0):
+            if butt[i].checkClicked():
+                print 'clicked'
     pygame.display.update()
-    
-def drawSlidBar(text,x,y,width,height,color,action,pos,bag_length):
-    # draws slide bar and defines event handling
-    cur = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    pygame.draw.rect(game_display,color,(x,y,width,height))
-    buttonText(text,(255,255,255),x,y,width,height)
-    if x + width > cur[0] > x and y + height > cur[1] > y:
-        if click[0] == 1 and action != None:
-            if action == 'up':
-                if pos > 0:
-                    clock.tick(10)
-                    return(-1)
-                
-            if action == 'down':
-                if pos < (bag_length/4)-4:
-                    clock.tick(10)
-                    return(1)
 
-    return(0)
-    
-    
-def drawButton(text,x,y,width,height,inactive_color,active_color,action=None):
-    # draws button and defines event handling
-    cur = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x + width > cur[0] > x and y + height > cur[1] > y:
-        pygame.draw.rect(game_display,active_color,(x,y,width,height))
-        if click[0] == 1 and action != None:
-            if action == 'play':
-                gameLoop()
-            if action == 'quit':
-                gameQuit()
+def drawScrollWindow(slide_pos):
+    width = 165
+    height = 145
+    x = (display_width-width-50)
+    y = (display_height-height-50)
+    s = pygame.Surface((width,height))
+    bag_slot = []
+    for i in range(60): bag_slot.append(buttons.button(str(i),x,y,30,30))#initalize bag slots
+    bag_length = len(bag_slot)
+    bar_size = ((height-40)/((bag_length/4)-3))+1
+    s.set_alpha(200)
+    s.fill(gray)
+    game_display.blit(s,(x,y))
 
-    else:
-        pygame.draw.rect(game_display,inactive_color,(x,y,width,height))
-    buttonText(text,(255,255,255),x,y,width,height)
+    slide_up =  buttons.button('/\\',x+width-20,y+5,15,15)#scroll up object
+    slide_up.drawButton(game_display)
+    slide_down = buttons.button('\\/',x+width-20,y+height-20,15,15)#scroll down object
+    slide_down.drawButton(game_display)
 
-def buttonText(msg,color,x,y,width,height,size='small'):
-    # centers text on buttons
-    textSurf, textRect = textObjects(msg,color,size)
-    textRect.center = ((x+(width/2)),y+(height/2))
-    game_display.blit(textSurf, textRect)
+    if pygame.mouse.get_pressed() == (1,0,0): #scroll bar event handling
+        if 0 < slide_pos:
+            if slide_up.checkClicked():
+                slide_pos -= 1
+        if slide_pos < ((bag_length/4)-4):
+            if slide_down.checkClicked():
+                slide_pos += 1
     
-def textObjects(text, color, size):
-    # font size handling and returns text on screen
-    if size == 'small':
-        textSurface = small_font.render(text, True, color)
-    elif size == 'medium':
-        textSurface = medium_font.render(text, True, color)
-    elif size == 'large':
-        textSurface = large_font.render(text, True, color)
-    return textSurface, textSurface.get_rect()
+    slide_bar = buttons.button('',x+width-20,y+20+(slide_pos*bar_size),15,bar_size)#create slide bar object
+    slide_bar.drawButton(game_display)#display slide bar
 
+    #display bag slots
+    for iy in range(4):
+        for ix in range(4):
+            bag_slot[(iy*4)+(slide_pos*4)+ix].setX(x+5+(ix*35))
+            bag_slot[(iy*4)+(slide_pos*4)+ix].setY(y+5+(iy*35))
+            bag_slot[(iy*4)+(slide_pos*4)+ix].drawButton(game_display)
+                                        
+    return(slide_pos)
+       
 def gameQuit():
     # quit game
     pygame.quit()
@@ -158,7 +117,6 @@ def gameLoop():
                     elif back_pack == False:
                         back_pack = True
 
-        #game_display.fill(white)
         game_display.blit(img,(0,0))
         if back_pack:
             slide_pos = drawScrollWindow(slide_pos)
